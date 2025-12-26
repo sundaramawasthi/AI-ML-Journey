bf71570ca0b3cb11a51a51a59e6e1b9397fd9949
SHIVAM AWASTHI <shivamawasthi@SHIVAMs-Mac-mini.local>
2025-12-26 21:46:46 +0530

    Add project README and Day-01 code

---
+++ Day-01 Pythonprogramming -BASIC/Chapter 1-variable and DataType.py
@@ -0,0 +1,76 @@
+# Day 1: Python Programming Basics
+# Topic: Introduction, Variables, and Data Types
+
+# Python is simple and easy to learn
+# Free and open source
+# High-level language
+# Portable (Windows, Mac, Linux)
+
+
+# OUR FIRST PROGRAM
+
+print("Namaste")
+
+# print() is a built-in function in Python
+# It is used to display output on the screen
+
+print("Hello Viewver here is your first program")
+
+# In the python we can use Charcter set in python
+# letter A to Z, a to z
+# Digit - 0 - 9
+# Special Symbol -+*/ etc
+# Also other charactere liike Blank space,tab, newline, ASCII and Unicode character as part of data. 
+
+print("Print in", "same line")
+
+# print Number in python
+
+print("I Print number", 45)
+
+# Arithmetic operations
+print("Addition =", 4 + 5)
+print("Subtraction =", 10 - 5)
+print("Division =", 5 / 6)
+print("Modulus =", 5 % 6)
+
+# VARIABLE
+#A variable is a name that given to a memory location in a program.
+
+#variable = value
+# for example 
+
+name = "sundram" #String
+
+#Here name is variable whose value is sundram like that
+
+age = 50 #Integer
+
+# Here age is variable and 50 is value, variable basically a memory location
+# Now we going to print the variable 
+
+print("my name is : ", name," and my age is",age )
+
+#print type of the variable name
+
+print("Type of name variable is ", type(name)) 
+
+# Lets write Data type like boolen and none
+
+old = True
+a = None
+
+print("Types of old ",type(old))
+print("Types of a is ", type(a))
+
+
+# Python program sum of two number
+#Basic code in python
+number1 = 3
+number2 = 5
+
+sum= number1+number2
+
+print("Sum of two number is ",sum)
+
+#python is a implicit language.
